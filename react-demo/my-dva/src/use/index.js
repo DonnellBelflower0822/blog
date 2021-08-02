f6d7fd29351198cb27705ec1e6c8589562fc0de6
xuxiaozhou <759811542@qq.com>
2021-08-02 15:16:12 +0800

    dva

---
+++ react-demo/my-dva/src/use/index.js
@@ -0,0 +1,38 @@
+import { pullAt } from 'lodash';
+import React from 'react';
+import dva from '../dva'
+import App from './App'
+
+const app = dva()
+app.model({
+  namespace: 'counter',
+  state: { count: 0 },
+  reducers: {
+    add(state) {
+      return { ...state, count: state.count + 1 }
+    }
+  },
+  effects: {
+    /**
+     * 
+     * @param {*} action 动作
+     * @param {*} effects saga/effects
+     */
+    *asyncAdd(action, { call }) {
+      yield call(delay, 1000)
+      yield pullAt({ type: 'add' })
+    }
+  }
+})
+
+app.router(() => <App />)
+
+app.start('#root')
+
+function delay(ms) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve()
+    }, ms)
+  })
+}
